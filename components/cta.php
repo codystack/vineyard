257700cbf86b65cb05cf32a68216c0e9a62381a1
codystack <ebuzzadvert@gmail.com>
2025-04-20 14:19:39 +0100

    Terms of use page added

--- components/cta.php
+++ components/cta.php
@@ -6,7 +6,7 @@
             <h2 class="h3">Interested in leasing a space in our mall or make enquiry about our event hall or props rental services?</h2>
           </div>
           <div class="col-md-4 text-center text-md-right">
-            <a href="rent-space" class="btn btn-outline-white ml-1">Book a Free Consultation</a>
+            <a href="rental" class="btn btn-outline-white ml-1">Book a Free Consultation</a>
           </div>
         </div>
       </div>
